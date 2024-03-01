0b40830164846474e19effcb542d037ca83b8ba8
Grouci Johnny <wwwgrouciyacine@gmail.com>
2024-03-01 12:08:25 +0100

    Update code to improve performance

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ app.use(passport.session())
 
 app.use(
     cors({
-        origin: "http://localhost:3000",
+        origin: "https://linkjob-cqn88bx1g-grouciyacine.vercel.app",
         methods: "GET,POST,PUT,DELETE",
         credentials: true,
     })
