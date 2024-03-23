d3ba15d31e07450a07b3973a66edba1d4e0360ed
Grouci Johnny <wwwgrouciyacine@gmail.com>
2024-03-23 16:06:38 +0100

    Updated login success route to handle authentication errors

--- index.js
+++ index.js
@@ -39,9 +39,10 @@ app.use((req, res, next) => {
 })
 //app.use(cors({ origin: 'https://linkjob-grouciyacine.vercel.app', credentials: true }));
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://linkjob-grouciyacine.vercel.app');//https://linkjob-grouciyacine.vercel.app
+    res.setHeader('Access-Control-Allow-Origin', 'https://linkjob-grouciyacine.vercel.app');
     res.setHeader('Access-Control-Allow-Credentials', 'true');
-    res.setHeader('Access-Control-Allow-Headers', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     next();
 });
 
