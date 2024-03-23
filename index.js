b4efb3f8f1694060ad1a4785ba1ab6a9cbf8e583
Grouci Johnny <wwwgrouciyacine@gmail.com>
2024-03-23 16:28:12 +0100

    Updated login success route to handle authentication errors

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use((req, res, next) => {
 })
 //app.use(cors({ origin: 'https://linkjob-grouciyacine.vercel.app', credentials: true }));
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://linkjob-liart.vercel.app');//https://linkjob-grouciyacine.vercel.app
+    res.setHeader('Access-Control-Allow-Origin', 'https://linkjob-grouciyacine.vercel.app');//https://linkjob-grouciyacine.vercel.app
     res.setHeader('Access-Control-Allow-Credentials', 'true');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
--- routes/Passport.js
+++ routes/Passport.js
@@ -1,6 +1,6 @@
 import passport from 'passport'
 import express from 'express';
-const CLIENT_URL = "https://linkjob-liart.vercel.app";
+const CLIENT_URL = "https://linkjob-grouciyacine.vercel.app";
 const app = express();
 
 app.get("/login/success", (req, res) => {
